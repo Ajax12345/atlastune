e48925ffcc4534680805056d3ec7418aaf239f6b
Ajax12345 <jpetullo14@gmail.com>
2024-03-30 14:00:25 -0400

    updates

---
+++ models.py
@@ -0,0 +1,20 @@
+import typing, numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optimizer
+import mysql_conn as db
+
+class Normalize:
+    @classmethod
+    def normalize(cls, arr:typing.List[float]) -> typing.List[float]:
+        mean = sum(arr)/len(arr)
+        std = pow(sum(pow(i - mean, 2) for i in arr)/len(arr), 0.5)
+        return [(i - mean)/std for i in arr]
+
+    
+if __name__ == '__main__':
+    with db.MySQL(database = "atlas_stuff") as conn:
+        metrics = db.MySQL.metrics_to_list(conn._metrics())
+        indices = db.MySQL.col_indices_to_list(conn.get_columns("test_stuff"))
+        print(Normalize.normalize([*indices, *metrics]))
+        
