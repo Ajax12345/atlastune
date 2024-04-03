d7a293bdb6ce3b250fa617ab339e6cc4b5e5108e
Ajax12345 <jpetullo14@gmail.com>
2024-04-03 08:27:20 -0400

    updates

---
+++ tpc/tpcc/queries/q10.sql
@@ -0,0 +1,10 @@
+select * from (select s.s_i_id, sum(case when 
+    s.s_quantity >= (select avg(s1.s_quantity) from stock s1 
+    join warehouse w on w.w_id = s1.s_w_id
+    join district d on d.d_w_id = w.w_id where d.d_city != '2HevADQC6IfvCXr6aOFt') 
+    then 1 else 0 end) a
+from stock s
+group by s.s_i_id) k
+join item i on i.i_id = k.s_i_id
+join order_line ol on ol.ol_i_id = i.i_id
+where ol.ol_delivery_d > '2024-03-31 17:54:14'
---
+++ tpc/tpcc/queries/q9.sql
@@ -0,0 +1,9 @@
+select c2.c_id, sum(case when c2.c_middle != 'Yb' then 1 else 0 end) from (
+    select c.c_id, c.c_last, c.c_middle from customer c
+    where c.c_balance >= (select avg(c1.c_balance) from customer c1 where c.c_first = c1.c_first)
+        and c.c_credit = 'BC'
+        and c.c_zip > 331277273
+        and c.c_delivery_cnt > 15
+) c2
+where substring(c2.c_last, 1, 4) = 'ESEE'
+group by c.c_id
